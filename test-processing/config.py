0a03fc56c8c65781948055fca9b254b4c54214ac
matthijsreyers <matthijsreyers@gmail.com>
2024-12-15 16:55:09 +0100

    Created more advanced analysis notebooks

--- test-processing/config.py
+++ test-processing/config.py
@@ -1,5 +1,5 @@
 
-SENSOR_DATA = 'recordings/bling2_standing.csv'
+SENSOR_DATA = 'recordings/bling2_walk.csv'
 ALGORITHM_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_algo.csv')
 ANGLES_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_angles.csv')
 STEPS_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_steps.csv')
--- test-processing/view_parts.py
+++ test-processing/view_parts.py
@@ -3,15 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, sys, math, random, time
 
-SENSOR_DATA = 'recordings/with_donanta.csv'
+SENSOR_DATA = 'recordings/donanta10.csv'
 ALGORITHM_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_algo.csv')
 ANGLES_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_angles.csv')
 STEPS_DATA = SENSOR_DATA.replace('recordings/', 'analysis/').replace('.csv', '_steps.csv')
 
 steps = pd.read_csv(SENSOR_DATA)
 # Convert ESP32 system time to local time so the graphs make more sense.
-now = time.time()
-steps['t'] = pd.to_datetime((steps['time'] / 1_000_000.0) + now, unit='s')
+steps['t'] = pd.to_datetime((steps['time'] / 1_000_000_000.0), unit='s')
 steps.set_index(steps['t'], inplace=True)
 steps.drop(columns=['t'], inplace=True)
 steps
@@ -21,7 +20,6 @@ fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 steps['accel.x'].plot(ax=ax1)
 steps['accel.y'].plot(ax=ax1)
 steps['accel.z'].plot(ax=ax1)
-ax1.xaxis.set_ticklabels([])
 ax1.legend(loc='upper left')
 ax1.grid()
 
